efc7cf8d9a41e2e5a1cf0758c4e859226b0ad4f6
Y-ARA25 <63275834+Y-ARA25@users.noreply.github.com>
2022-06-18 17:56:15 -0500

    18JUN second commit

---
+++ week1/selectingElements/scriptsheet.js
@@ -0,0 +1,13 @@
+// selecting a single element => returns an object
+
+//selecting multiple elements => returns an array
+//id has to be VERY specific
+//document = html file
+// . = object dot notation
+//getElementById = is appeneded to retrieve an element from the document
+
+
+document.getElementById("h1").innerText = "This is an h1 tag"
+
+
+
